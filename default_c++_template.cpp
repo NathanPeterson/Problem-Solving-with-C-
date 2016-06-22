edf9090a85d93a06139fa5c3d7afd9c7ea2a1a8b
NathanPeterson <nalexpeterson@gmail.com>
2016-06-21 21:35:21 -0400

    Add statements to default template to show the start/end of problem

--- default_c++_template.cpp
+++ default_c++_template.cpp
@@ -3,5 +3,10 @@ using namespace std;
 
 int main(){
 
+  cout << "\n\t############# STARTING PROBLEM ##.## #############\n\n";
+
+
+  cout << "\n\t############# ENDING PROBLEM ##.## #############\n\n";
+
   return 0;
 }
