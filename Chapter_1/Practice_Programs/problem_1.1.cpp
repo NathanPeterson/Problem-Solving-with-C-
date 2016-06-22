7115e2e4dcc86cb1111330b7527933203f5ee51f
NathanPeterson <nalexpeterson@gmail.com>
2016-06-21 21:38:04 -0400

    Solution for Problem 1.1

---
+++ Chapter_1/Practice_Programs/problem_1.1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int number_of_pods, peas_per_pod, total_peas;
+
+  cout << "\n\t############# STARTING PROBLEM 1.1 #############\n\n";
+
+  cout << "Press return after entering a number.\n";
+  cout << "Enter the number of pods:\n";
+  cin >> number_of_pods;
+
+  cout << "Enter the number of peas in a pod:\n";
+  cin >> peas_per_pod;
+
+  total_peas = number_of_pods * peas_per_pod;
+
+  cout << "\nIf you have ";
+  cout << number_of_pods;
+  cout << " pea pods \n";
+  cout << "and ";
+  cout << peas_per_pod;
+  cout << " peas in each pod, then\n";
+  cout << "you have ";
+  cout << total_peas;
+  cout << " peas in all the pods.\n";
+
+  cout << "\n\t############# ENDING PROBLEM 1.1 #############\n\n";
+
+  return 0;
+}
