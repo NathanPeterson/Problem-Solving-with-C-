721338c2d9126754bb954f512bd94bba59e5f121
NathanPeterson <nalexpeterson@gmail.com>
2016-06-22 22:05:41 -0400

    Update Default Template to include a header

--- default_c++_template.cpp
+++ default_c++_template.cpp
@@ -1,12 +1,18 @@
+// File Name: .cpp
+// Author: Nathan Peterson
+// Email Address: nalexpeterson@gmail.com
+// Description:
+// Last Changed: , 2016
+
+
 #include<iostream>
 using namespace std;
 
 int main(){
-
   cout << "\n\t############# STARTING PROBLEM ##.## #############\n\n";
-
+  cout << "\n\tProblem: "
+  cout << "\n"
 
   cout << "\n\t############# ENDING PROBLEM ##.## #############\n\n";
-
   return 0;
 }
